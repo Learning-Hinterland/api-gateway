5a03f3f0e42328cb2b3fef4f9eed1ad399bd51b6
Lala <nadhifahsalsabila82@gmail.com>
2024-06-17 17:58:39 +0700

    update middleware format

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -6,7 +6,12 @@ const jwt = require('jsonwebtoken');
 async function restrict(req, res, next) {
     const token = req.headers.authorization;
     if (!token || !token.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'No token provided' });
+        return res.status(401).json({
+            status: false,
+            message: 'No token provided',
+            error: null,
+            data: null
+        });
     }
 
     try {
@@ -14,20 +19,35 @@ async function restrict(req, res, next) {
         req.user = decoded;
         next();
     } catch (error) {
-        return res.status(401).json({ message: 'Invalid token' });
+        return res.status(401).json({
+            status: false,
+            message: 'Invalid token',
+            error: null,
+            data: null
+        });
     }
 };
 
 async function isAdmin(req, res, next) {
     if (req.user.role !== 'ROLE_ADMIN') {
-        return res.status(403).json({ message: 'Forbidden' });
+        return res.status(403).json({
+            status: false,
+            message: 'Access denied. Only admin can access this route',
+            error: null,
+            data: null
+        });
     }
     next();
 }
 
 async function isLecturer(req, res, next) {
     if (req.user.role !== 'ROLE_LECTURER') {
-        return res.status(403).json({ message: 'Forbidden' });
+        return res.status(403).json({
+            status: false,
+            message: 'Access denied. Only lecturer can access this route',
+            error: null,
+            data: null
+        });
     }
     next();
 }
